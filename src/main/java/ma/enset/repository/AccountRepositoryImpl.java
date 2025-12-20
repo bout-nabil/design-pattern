943240819e0099e21df513b6dc1c1a0dc2e6612a
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-12-20 12:06:58 +0100

    create repository

---
+++ src/main/java/ma/enset/repository/AccountRepositoryImpl.java
@@ -0,0 +1,67 @@
+package ma.enset.repository;
+
+import ma.enset.model.BankAccount;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class AccountRepositoryImpl implements AccountRepository {
+    private Map<Long, BankAccount> bankAccountMap = new HashMap<>();
+    private Long accountIdSequence = 0L;
+    @Override
+    public BankAccount save(BankAccount bankAccount) {
+        Long accountId = ++accountIdSequence;
+        bankAccount.setIdBankAccount(accountId);
+        bankAccountMap.put(accountId, bankAccount);
+        return bankAccount;
+    }
+
+    @Override
+    public List<BankAccount> findAll() {
+        return bankAccountMap.values().stream().toList();
+    }
+
+    @Override
+    public Optional<BankAccount> findById(Long id) {
+        BankAccount account = bankAccountMap.get(id);
+        if (account == null) {
+            return Optional.empty();
+        }else {
+            return Optional.of(account);
+        }
+    }
+
+    @Override
+    public List<BankAccount> searchAccounts(Predicate<BankAccount> predicate) {
+        return bankAccountMap.values().stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public BankAccount update(BankAccount bankAccount) {
+        bankAccountMap.put(bankAccount.getIdBankAccount(), bankAccount);
+        return bankAccount;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bankAccountMap.remove(id);
+    }
+
+    public void populateTestData() {
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}
+
+
+
+
+
+
